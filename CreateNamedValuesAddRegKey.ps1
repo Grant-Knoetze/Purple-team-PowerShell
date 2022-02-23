9dac1c5f17246e81f32011044209afdf29f73834
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-02-23 19:44:33 +0200

    Complete CreateNamedValuesAddRegKey.ps1

--- CreateNamedValuesAddRegKey.ps1
+++ CreateNamedValuesAddRegKey.ps1
@@ -2,4 +2,7 @@
 
 $regpath ="HKCU:SoftwareMyCustomSoftware"
 $regname ="BuildTime"
-
+$regvalue ="Build Started At: " + "" +(Get-Date)
+New-ItemProperty -Path $regpath -Name $regname -PropertyType String -Value $regvalue
+$verifyValue = Get-ItemProperty -Path $regpath -Name $regname
+Write-Host"The$regname named value is set to:"$verifyValue.$regname
