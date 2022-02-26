2b934ed2f2991202ff4cb46e9cda9e5d5f70dbf7
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-02-26 14:20:34 +0200

    Add three new files
    Add three new files including a script to assist with exporting procmon
    logs in XML format.

---
+++ ExportToXml.ps1
@@ -0,0 +1,2 @@
+# Navigate to the folder containing procmon etc and run this as admin
+.\Procmon.exe /OpenLog .\Logfile.PML /SaveAs .\Logfile.XML
---
+++ Untitled1.ps1
@@ -0,0 +1,1 @@
+﻿Write-Host Congratulations! Your first script executed successfully.
